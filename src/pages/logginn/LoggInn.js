6486805c716180c5e38c72ef33811e3b2606752b
signesgu <signesgu@stud.ntnu.no>
2024-02-09 12:44:13 +0100

    feat #6. enter the elements to be displayed on the login page

--- src/pages/logginn/LoggInn.js
+++ src/pages/logginn/LoggInn.js
@@ -2,7 +2,25 @@ import React from 'react'
 
 const LoggInn = () => {
   return (
-    <div>LoggInn</div>
+    <div className='logInContainer'>
+      <div className='logInHeader'>
+        <h1>Logg inn her:</h1>
+      </div>
+
+      <form>
+        <label for='username'>Brukernavn:</label>
+        <input type="text" id="username" name='username'></input>
+
+        <label for='password'>Passord:</label>
+        <input type="password" id="password" name='password'></input>
+
+        <label for="email">Epost:</label>
+        <input type="email" id="email" name="email"></input>
+        
+        <button type='button'>Logg inn</button>
+      </form>
+
+    </div>
   )
 }
 
