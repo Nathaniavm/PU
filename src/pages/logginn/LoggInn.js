100ee3795c4271671b36ee81324c429b9bc001b6
Aleksander <alekf@stud.ntnu.no>
2024-02-14 18:08:49 +0100

    Added login validation template functions

--- src/pages/logginn/LoggInn.js
+++ src/pages/logginn/LoggInn.js
@@ -9,15 +9,43 @@ const LoggInn = () => {
 
   const handleLogin = () => {
     if(!IsLoggedIn){
+
       const username = document.getElementById('username').value;
-      //Login logic for database
-      login(username); //if the login from database is OK
+      const password = document.getElementById('password').value;
+      const email = document.getElementById('email').value;
+
+      console.log(username, password, email);
+
+      if(!userExists(username)){
+        alert("Bruker finnes ikke")
+      }
+
+      else if(!passwordMatch(username, password)){
+        alert("Feil passord")
+      }
+
+      else {
+        login(username);
+
+        //Return to home page on a successful login
+        window.location.replace("/");
+      }
     }
 
   }
 
   const handleLogout = () => {
     logout();
+  }
+
+  const userExists = (username) => {
+    //Check if username is registered in the database
+    return true;
+  }
+
+  const passwordMatch = (username, password) => {
+    //Check if password matches username in database
+    return true;
   }
 
 
