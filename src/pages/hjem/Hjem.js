a0f159051811a89bafb2e3041a9f8059a9057878
Mathias Skogen-Karstensen <mathiajs@stud.ntnu.no>
2024-02-17 19:43:07 +0100

    feat #21: Add functionality to search for games, also added filter function, since they both used the same logic

--- src/pages/hjem/Hjem.js
+++ src/pages/hjem/Hjem.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Hjem.css'
 import myImage from './reklame.png';
 import { Link } from 'react-router-dom';
@@ -6,13 +6,29 @@ import { Link } from 'react-router-dom';
 
 
 const placeholderGames = [
-  {gameID: 1, title: 'Stiv Heks', description: 'bladdi bladdi bla', category: 'fysisk lek', nPeople: '10'},
-  {gameID: 2, title: 'Navnedyrleken', description: 'blææææææææ', category: 'icebreaker', nPeople: '1'},
-  {gameID: 3, title: 'Klumpelure', description: 'blåååpopppppepepewpepe', category: 'tullete', nPeople: '4'},
-  {gameID: 4, title: 'BONGONG', description: 'fjsdhkfhdsufheuh', category: 'tullete', nPeople: '4'},
+  {gameID: 1, title: 'Stiv Heks', description: 'En blir valgt til å være heks, heksa skal løpe etter de andre og prøve å ta på dem, hvis man blir truffet av heksa må man stå med beina spredt, og man blir fri hvis noen kraber under beina dine'
+  , category: 'fysisk lek', nPeople: '10'},
+  {gameID: 2, title: 'Navnedyrleken', description: 'Alle sier navnet sitt, og et dyr med samme forbokstav som navnet', category: 'navnelek', nPeople: '1'},
+  {gameID: 3, title: 'Spille kort', description: 'Bare spille ett eller annet med kort', category: 'icebreaker', nPeople: '4'},
+  {gameID: 4, title: 'Sista', description: 'Løpe etter hverandre', category: 'fysisk lek', nPeople: '4'},
 ]
 
 const Hjem = () => {
+    const [searchInput, setSearchInput] = useState('')
+    const [selectedCategory, setCategory] = useState('alle')
+
+    const handleSearchChange = (e) => {
+      setSearchInput(e.target.value);
+    }
+
+    const handleCategoryChange = (e) => {
+      setCategory(e.target.value);
+    }
+
+    const filteredGames = placeholderGames.filter( game =>
+      (game.title.toLowerCase().includes(searchInput.toLowerCase()) || searchInput === '') &&
+      (selectedCategory === 'alle' || game.category === selectedCategory)
+      );
   return (
     <>
     <div className='classContainer'>
@@ -32,23 +48,37 @@ const Hjem = () => {
             <h4>Søk etter leker:</h4>
             <form>
               <label for="search"> </label>
-              <input className= 'homeInputField' type='search' id='search' name='search'></input>
+              <input 
+                className= 'homeInputField' 
+                type='search' 
+                id='search' 
+                name='search'
+                value={searchInput}
+                onChange={handleSearchChange}
+                placeholder='Søk etter tittel'
+                />
             </form>
           </div>
 
           <div className='filter'>
             <h4>Velg type lek:</h4>
-            <select className='homeInputField' id='choiceBox'>
-              <option value='option1'>Navnelek</option>
-              <option value='option2'>Icebreaker</option>
-              <option value='option3'>Fysisk lek</option>
+            <select
+              className='homeInputField' 
+              id='choiceBox'
+              value={selectedCategory}
+              onChange={handleCategoryChange}
+                >
+                <option value='alle'>Alle</option>
+                <option value='navnelek'>Navnelek</option>
+                <option value='icebreaker'>Icebreaker</option>
+                <option value='fysisk lek'>Fysisk lek</option>
             </select>
           </div>
 
           <div className='gameOverview'>
             <h4>Bli kjent-leker:</h4>
             <div className='gameGrid'>
-              {placeholderGames.map((game, index) => (
+              {filteredGames.map((game, index) => (
                 <Link to={`game/${game.gameID}`} key={index} className="gameSquare">
                   <h4>{game.title}</h4>
                   <div className="gameSquare-p-content">
