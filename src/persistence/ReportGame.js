538ead19fe0df3ba548c49d5795c58903c8e7c55
Aleksander <alekf@stud.ntnu.no>
2024-02-22 00:36:06 +0100

    Created ReportGame file and added To-Do list

---
+++ src/persistence/ReportGame.js
@@ -0,0 +1,5 @@
+//TODO:
+//Opprett tabell for rapportertAv-relasjon og funksjon for innsetting
+//UserID [int], GameID[int]
+//Sjekke om bruker allerede har rapportert lek
+//Hente ut antall rapporteringer for en gitt lek
